8a9a0946d09d4709b23addd42dc934c023f7afd3
Nate Holland <nateholland6@gmail.com>
2015-02-14 11:18:19 -0500

    updating footer credit

--- wp-content/themes/julialandauer/footer.php
+++ wp-content/themes/julialandauer/footer.php
@@ -12,9 +12,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'julialandauer' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'julialandauer' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'julialandauer' ), 'julialandauer', '<a href="http://nathanielholland.com" rel="designer">Nate Holland</a>' ); ?>
+			<p>Design and Development by <a href="http://nathanielholland.com">Nate Holland</a></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
