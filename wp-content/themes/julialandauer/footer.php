217662ab54a78a55420b196f34284e5803397cee
Nate Holland <nateholland6@gmail.com>
2015-03-07 15:34:50 -0500

    setting up footer and breaking header/footer styles into partials

--- wp-content/themes/julialandauer/footer.php
+++ wp-content/themes/julialandauer/footer.php
@@ -11,9 +11,17 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
+		<div class="site-footer__credits">
 			<p>Design and Development by <a href="http://nathanielholland.com">Nate Holland</a></p>
-		</div><!-- .site-info -->
+		</div><!-- .site-footer -->
+
+		<div class="site-footer__social">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/social/facebook.svg" width="35px" alt="facebook">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/social/twitter.svg" width="35px" alt="twitter">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/social/instagram.svg" width="35px" alt="instagram">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/social/youtube.svg" width="35px" alt="youtube">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/social/linkedin.svg" width="35px" alt="linkedin">
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
