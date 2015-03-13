e5f3778f7f80fdd066264bfd56acaae4bca73023
Nate Holland <nateholland6@gmail.com>
2015-03-12 21:05:06 -0400

    removing page header and edit link

--- wp-content/themes/julialandauer/content-page.php
+++ wp-content/themes/julialandauer/content-page.php
@@ -7,9 +7,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content(); ?>
@@ -20,8 +17,4 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php edit_post_link( __( 'Edit', 'julialandauer' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
