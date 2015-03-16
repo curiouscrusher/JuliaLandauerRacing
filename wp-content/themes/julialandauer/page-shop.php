ad4fea9e27ec64662a515a123c039309cff0213e
Nate Holland <nateholland6@gmail.com>
2015-03-15 23:13:23 -0400

    shop page final layout and paypal buttons

--- wp-content/themes/julialandauer/page-shop.php
+++ wp-content/themes/julialandauer/page-shop.php
@@ -18,47 +18,99 @@ get_header(); ?>
 					<div class="shop-page__wrapper">
 						
 							<div class="shop-page__hero-card">
-								<h2 class="shop-page__hero-card--title">Hero Card</h2>
-									<p>
+								<!-- <h2 class="shop-page__hero-card- -title">Hero Card</h2> -->
+									<p class="shop-page__hero-card--description">
 										<!-- Load the hero card text -->
 										<?php  echo get_post_meta($post->ID, 'hero-card-text', true); ?>
+
+										<button>Order Now</button>
 									</p>
+
+									<img src="<?php echo site_url(); ?>/wp-content/uploads/<?php  echo get_post_meta($post->ID, 'shop-page-hero-card-front', true); ?>" class="shop-page__hero-card--image" />
+
 							</div>
+
 							<div class="shop-page__water-bottle">
-								<!-- <h2 class="shop-page__water-bottle--title">JLR Water Bottle</h2> -->
-									<p class="shop-page__water-bottle--description">
+								
+									<div class="shop-page__water-bottle--description">
+										<h2>JLR Water Bottle</h2>
 										<!-- Load the water bottle text -->
 										<?php  echo get_post_meta($post->ID, 'water-bottle-text', true); ?>
 
-										<button>Order Now</button>
-									</p>
+										<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+											<input type="hidden" name="cmd" value="_s-xclick">
+											<input type="hidden" name="hosted_button_id" value="MN3DQWSMWH6RG">
+												<button>
+													<input type="submit" value="Order Now" border="0" name="submit" alt="">
+												</button>
+											<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+										</form>
+									</div>
 
-									<img class="shop-page__water-bottle--image" src="http://placehold.it/600x300">
 
-									<!-- <img src="<?php echo site_url(); ?>/wp-content/uploads/<?php  echo get_post_meta($post->ID, 'water-bottle', true); ?>" class="shop-page__water-bottle--bottle" /> -->
+
+									<img class="shop-page__water-bottle--image" src="<?php echo site_url(); ?>/wp-content/uploads/jlr-bottle.jpg">
+
+									
 							</div>
+
 							<div class="shop-page__hat">
-								<!-- <h2 class="shop-page__hat--title">JLR Hat</h2> -->
-									<p class="shop-page__hat--description">
+								
+									<div class="shop-page__hat--description">
+										<h2>JLR Hat</h2>
 										<!-- Load the hat text -->
 										<?php  echo get_post_meta($post->ID, 'hat-text', true); ?>
 
-										<button>Order Now</button>
-									</p>
+										<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+											<input type="hidden" name="cmd" value="_s-xclick">
+											<input type="hidden" name="hosted_button_id" value="8JDRV4F5KXLLA">
+												<button>
+													<input type="submit" value="Order Now" border="0" name="submit" alt="">
+												</button>
+											<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+										</form>
+									</div>
 
-									<img class="shop-page__hat--image" src="http://placehold.it/600x300">
+									<img class="shop-page__hat--image" src="<?php echo site_url(); ?>/wp-content/uploads/jlr-hat.jpg">
 							</div>
+
 							<div class="shop-page__shirt">
-								<!-- <h2 class="shop-page__shirt--title">JLR Shirt</h2> -->
-									<p class="shop-page__hat--description">
+								
+									<div class="shop-page__shirt--description">
+										<h2>JLR Shirt</h2>
 										<!-- Load the shirt text -->
 										<?php  echo get_post_meta($post->ID, 'shirt-text', true); ?>
 
-										<button>Order Now</button>
-									</p>
+										<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+											<input type="hidden" name="cmd" value="_s-xclick">
+											<input type="hidden" name="hosted_button_id" value="5QGKJQMMSN9LU">
+												<button>
+													<input type="submit" value="Order Now" border="0" name="submit" alt="">
+												</button>
+											<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+											<table>
+												<tr>
+													<td>
+														<input type="hidden" name="on0" value="Sizes">Sizes
+													</td>
+												</tr>
+												<tr>
+													<td>
+														<select name="os0">
+														    <option value="Small">Small </option>
+														    <option value="Medium">Medium </option>
+														    <option value="Large">Large </option>
+														    <option value="Extra Large">Extra Large </option>
+														</select>
+													</td>
+												</tr>
+											</table>
+										</form>
+									</div>
 
-									<img class="shop-page__hat--image" src="http://placehold.it/600x300">
+									<img class="shop-page__shirt--image" src="<?php echo site_url(); ?>/wp-content/uploads/jlr-shirt.jpg">
 							</div>
+
 					</div>				
 
 				<?php endwhile; // end of the loop. ?>
