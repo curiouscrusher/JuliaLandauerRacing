59c1e166bd87855fc5d231f6d6bee1ac2d51d491
Nate Holland <nateholland6@gmail.com>
2015-03-19 23:32:48 -0400

    added mailchimp form and contact details

--- wp-content/themes/julialandauer/page-contact.php
+++ wp-content/themes/julialandauer/page-contact.php
@@ -29,7 +29,53 @@ get_header(); ?>
 									
 							</div>
 							<div class="contact-page__wrapper--info-section">	
-									
+									<p>Email</p>
+									<p>
+									<!-- Load the email address text -->
+									<?php  echo get_post_meta($post->ID, 'contact-email', true); ?>
+									</p>
+
+									<p>Phone</p>
+									<p>
+									<!-- Load the phone number -->
+									<?php  echo get_post_meta($post->ID, 'contact-phone', true); ?>
+									</p>
+
+									<p>Address</p>
+									<p>
+									<!-- Load the address -->
+									<?php  echo get_post_meta($post->ID, 'contact-address', true); ?>
+									</p>
+
+									<!-- Begin MailChimp Signup Form -->
+									<div id="mc_embed_signup">
+										<form action="//julialandauer.us9.list-manage.com/subscribe/post?u=1ec5ea0db145448aa433052d1&amp;id=9fb1ba425c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+											<div id="mc_embed_signup_scroll">
+
+												<div class="mc-field-group form-group">
+													<input type="text" name="FNAME" class="required form-control" id="mce-FNAME" placeholder="Your Name">
+												</div>
+
+												<div class="mc-field-group form-group">
+													<input type="email" name="EMAIL" class="required email form-control" id="mce-EMAIL" placeholder="Your Email">
+												</div>
+												<div id="mce-responses" class="clear">
+													<div class="response" id="mce-error-response" style="display:none"></div>
+													<div class="response" id="mce-success-response" style="display:none"></div>
+												</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+
+												<div style="position: absolute; left: -5000px;">
+													<input type="text" name="b_1ec5ea0db145448aa433052d1_9fb1ba425c" tabindex="-1" value="">
+												</div>
+												<div class="clear">
+													<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-large">
+												</div>
+											</div>
+
+										</form>
+									</div>
+									<!--End MailChimp Signup Form -->
+
 							</div>
 
 					</div>
