a1af77a92c75d5db94253ed61a88c5b7e7d86d75
Nate Holland <nateholland6@gmail.com>
2015-03-22 23:13:26 -0400

    rough media page layout

--- wp-content/themes/julialandauer/page-media.php
+++ wp-content/themes/julialandauer/page-media.php
@@ -17,23 +17,45 @@ get_header(); ?>
 					
 					<?php get_template_part( 'content', 'page' ); ?>
 
+					<div class="media-page__youtube-wrapper">
+
+					</div>
+
 					<div class="media-page__photos">
 						<h1 class="media-page__photos--title">Photos</h1>
 						<?php putRevSlider(6) ?>
+					</div>
+
+					<div class="media-page__survivor-wrapper">
+						<h2>Survivor</h2>
+						
+						<div class="media-page__survivor-wrapper--left">
+							<p>
+								<!-- Load the left survivor text -->
+								<?php  echo get_post_meta($post->ID, 'survivor-left', true); ?>
+							</p>
+						</div>
+						<div class="media-page__survivor-wrapper--right">
+							<p>
+								<!-- Load the right survivor text -->
+								<?php  echo get_post_meta($post->ID, 'survivor-right', true); ?>
+							</p>
+						</div>
 					</div>
 
 					<div class="media-page__blocks">
 							<div class="media-page__blocks--huffington">
 								<h2>Huffington Post</h2>
 									<p>
-										<!-- Load -->
-										<a href="#"><button>Read More</button></a>
+										<!-- Load the huff post blurb -->
+										<?php  echo get_post_meta($post->ID, 'huff-post', true); ?>
 									</p>
+									<a href="#"><button>Read More</button></a>
 							</div>
 							<div class="media-page__blocks--media-inquiry">
 								<h2>Media Inquiry</h2>	
 									<p>
-										<!-- Load the media text -->
+										<!-- Load the media inquiry text -->
 										<?php  echo get_post_meta($post->ID, 'media-inquiry', true); ?>
 									</p>
 									<a href="<?php echo site_url(); ?>/contact"><button>Contact</button></a>
