c3104a5fe3ad6ac27d8497511136c9ef5424c248
Nate Holland <nateholland6@gmail.com>
2015-04-08 23:09:26 -0400

    stying mobile menu

--- wp-content/themes/julialandauer/header.php
+++ wp-content/themes/julialandauer/header.php
@@ -34,7 +34,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'julialandauer' ); ?></button>
+			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
