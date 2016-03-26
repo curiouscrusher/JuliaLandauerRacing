4ce94ecf63b4cee6b4a495932397f86619790514
Nate Holland <nate@gaugeinteractive.com>
2016-03-26 07:56:12 -0400

    Removing announcement bar

--- wp-content/themes/julialandauer/header.php
+++ wp-content/themes/julialandauer/header.php
@@ -70,9 +70,9 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 
-<div class="announcement-bar">
-  <h5>Irwindale Speedway race will be televised on Friday, March 25 at 9 pm EST on NBCSN</h5>
-</div>
+<!-- <div class="announcement-bar">
+  <h5></h5>
+</div> -->
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
