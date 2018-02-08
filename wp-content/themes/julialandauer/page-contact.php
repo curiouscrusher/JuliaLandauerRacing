149680fd77e278455933817dc49d43a2d0d765a4
Nate Holland <>
2018-02-07 21:38:27 -0600

    Add new contact form shortcode and down caret svg

--- wp-content/themes/julialandauer/page-contact.php
+++ wp-content/themes/julialandauer/page-contact.php
@@ -25,7 +25,7 @@ get_header(); ?>
 								<br /><br />
 								We will try to respond within 3-5 business days.</p>
 
-								<?php echo do_shortcode( '[contact-form-7 id="91" title="julia landauer contact"]' ); ?>
+								<?php echo do_shortcode( '[contact-form-7 id="626" title="julia landauer contact new"]' ); ?>
 									
 							</div>
 							<div class="contact-page__wrapper--info-section">	
